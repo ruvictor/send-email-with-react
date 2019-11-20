a88ef9e0ad0e86f9529114e88629cc7dee90ea30
Victor <rusuvictor30@gmail.com>
2019-11-19 20:31:21 -0500

    API file added

---
+++ api.ruvictor.com/index.php
@@ -0,0 +1,51 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// get refferer server
+if($_SERVER['HTTP_REFERER'] === "http://localhost:3000/"){
+    // extract the data from $_POST
+    $name = isset($_GET['name']) ? $_GET['name'] : null;
+    $message = isset($_GET['message']) ? $_GET['message'] : null;
+    $email = isset($_GET['sendto']) ? $_GET['sendto'] : null;
+
+    if($name && $message && $email){
+    
+        //Load composer's autoloader
+        require 'vendor/autoload.php';
+
+        $mail = new PHPMailer(true);
+        try{
+            // SMTP server configuration
+            $mail->isSMTP();                                      // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                             // Enable SMTP authentication
+            $mail->Username   = '';           // SMTP username
+            $mail->Password   = '';                        // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 587;
+
+            // Recipients
+            $mail->setFrom('mitrodigitalsmtp@gmail.com', 'React Contact form');
+            $mail->addAddress($email);     // Add a recipient
+            $mail->addReplyTo('mitrodigitalsmtp@gmail.com', 'Information');
+
+            // Content
+            $mail->isHTML(true);      // Set email format to HTML
+            $mail->Subject = 'React Contact form';
+            $mail->Body    = 'Name: ' . $name . '<br />Email: ' . $email . '<br /><br /><b>Message:</b> '
+            . $message;
+
+            if($mail->send())
+                echo "Message has been sent!";
+        }catch (Exception $e){
+            echo "Message couldn't be sent. Error: ", $mail->ErrorInfo;
+        }
+    }else{
+        echo "All the fileds are required!";
+    }
+}else{
+    echo "You can't use this server!";
+}
+?>
